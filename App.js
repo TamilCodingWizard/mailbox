92170d9bf420600d8b174b6450fdf2c38927073a
tamilCodingWizard <tamilcodingwizard@gmail.com>
2022-09-19 16:22:35 +0100

    video5

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
-import { ActivityIndicator, Button,  } from '@react-native-material/core';
-import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import Inbox from './screens/Inbox';
-import { InboxStack } from './routes/InboxStack';
+import { ActivityIndicator, Button } from "@react-native-material/core";
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import Inbox from "./screens/Inbox";
+import { InboxStack } from "./routes/InboxStack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { DraftsStack } from "./routes/DraftsStack";
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style='auto'/>
+      <StatusBar style="auto" />
       <NavigationContainer>
-        <InboxStack/>
+        <Drawer.Navigator screenOptions={{headerShown:false}}>
+          <Drawer.Screen name="Inbox" component={InboxStack} />
+          <Drawer.Screen name="Drafts" component={DraftsStack} />
+        </Drawer.Navigator>
       </NavigationContainer>
     </>
   );
@@ -19,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ routes/DraftsStack.js
@@ -0,0 +1,15 @@
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Drafts from './../screens/Drafts';
+
+const Stack = createNativeStackNavigator()
+
+export const DraftsStack = () => {
+
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="DraftsScreen" component={Drafts}/>
+        </Stack.Navigator>
+    )
+
+}
